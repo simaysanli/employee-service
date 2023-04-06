15c000f04806b48251691e5c2f4a482a51037999
Simay Sanli <sanli.simay@gmail.com>
2023-04-06 21:40:29 +0200

    Initial commit

---
+++ src/test/java/com/justeat/interview/employeeservice/EmployeeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justeat.interview.employeeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
