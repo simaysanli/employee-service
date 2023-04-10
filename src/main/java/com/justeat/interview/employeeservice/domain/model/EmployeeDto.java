81e321114313a0de7f5cc7f79ec73be607684159
Simay Sanli <sanli.simay@gmail.com>
2023-04-10 22:52:04 +0200

    add documentation for swagger open api

--- src/main/java/com/justeat/interview/employeeservice/domain/model/EmployeeDto.java
+++ src/main/java/com/justeat/interview/employeeservice/domain/model/EmployeeDto.java
@@ -2,7 +2,9 @@ package com.justeat.interview.employeeservice.domain.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -18,12 +20,20 @@ public class EmployeeDto {
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private UUID id;
+
+    @Size(min = 0, max = 20)
     private String firstName;
+
+    @Size(min = 0, max = 20)
     private String lastName;
+
     private Set<Hobby> hobbies;
+
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @Schema(type = "string", pattern = "yyyy-MM-dd", example = "2023-04-10")
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthday;
+
     @Email(regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}")
     private String email;
 }
--- src/main/java/com/justeat/interview/employeeservice/resources/EmployeeController.java
+++ src/main/java/com/justeat/interview/employeeservice/resources/EmployeeController.java
@@ -3,6 +3,11 @@ package com.justeat.interview.employeeservice.resources;
 import com.justeat.interview.employeeservice.domain.model.EmployeeDto;
 import com.justeat.interview.employeeservice.entity.Employee;
 import com.justeat.interview.employeeservice.services.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,6 +31,13 @@ public class EmployeeController {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Operation(summary = "Get all employees")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found all employees",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))}),
+            @ApiResponse(responseCode = "404", description = "Employees not found",
+                    content = @Content)})
     @GetMapping
     @Valid
     public List<EmployeeDto> getEmployees() {
@@ -35,6 +47,13 @@ public class EmployeeController {
                 .collect(Collectors.toList());
     }
 
+    @Operation(summary = "Create an employee")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee created successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))}),
+            @ApiResponse(responseCode = "500", description = "Employee not created",
+                    content = @Content)})
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Employee addEmployee(@Valid @RequestBody EmployeeDto employeeDto) {
@@ -42,17 +61,44 @@ public class EmployeeController {
         return employeeService.addEmployee(employee);
     }
 
+    @Operation(summary = "Get an employee by id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found an employee",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content)})
     @GetMapping(value = "{employeeId}")
     public EmployeeDto getEmployeeById(@PathVariable String employeeId) {
         return convertToDto(employeeService.getEmployeeById(employeeId));
     }
 
+    @Operation(summary = "Update an existing employee")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee updated successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content)})
     @PutMapping(value = "{employeeId}")
     public ResponseEntity<Employee> updateEmployee(@PathVariable UUID employeeId, @RequestBody EmployeeDto employeeDto) {
         Employee updatedEmployee = convertToEntity(employeeDto);
         return employeeService.updateEmployeeById(String.valueOf(employeeId), updatedEmployee);
     }
 
+    @Operation(summary = "Delete an existing employee")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee deleted successfully",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = EmployeeDto.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content)})
     @DeleteMapping(value = "{employeeId}")
     public ResponseEntity<Void> removeEmployeeById(@PathVariable UUID employeeId) {
         return employeeService.removeEmployeeById(String.valueOf(employeeId));
